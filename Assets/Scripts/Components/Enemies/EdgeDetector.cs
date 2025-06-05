f882efa97bd6acdd9abf391cee47ad97f45f48da
Argedia <ariel2010gh@gmail.com>
2025-06-05 13:38:51 -0500

    rama rescatada

---
+++ Assets/Scripts/Components/Enemies/EdgeDetector.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EdgeDetector : MonoBehaviour
+{
+    [Header("Detección de borde")]
+    public Vector2 rayOriginOffset = new Vector2(0.5f, -0.5f); // Offset local respecto al transform
+    public float checkDistance = 0.1f;
+    public LayerMask groundLayer;
+
+    [Header("Estado")]
+    public bool isGroundAhead = true;
+
+    void FixedUpdate()
+    {
+        isGroundAhead = CheckGroundAhead();
+    }
+
+    bool CheckGroundAhead()
+    {
+        Vector2 origin = (Vector2)transform.position + rayOriginOffset;
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, checkDistance, groundLayer);
+        return hit.collider != null;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector2 origin = (Vector2)transform.position + rayOriginOffset;
+        Gizmos.color = isGroundAhead ? Color.green : Color.red;
+        Gizmos.DrawLine(origin, origin + Vector2.down * checkDistance);
+    }
+}
