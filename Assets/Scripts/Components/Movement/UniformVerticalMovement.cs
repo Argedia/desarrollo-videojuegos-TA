6e94246b3a81615ac968a289d727d08683cf6e39
AAlvaradoE <aalvaradoe@pucp.edu.pe>
2025-06-24 15:30:45 -0500

    feat: bat & mushroom

---
+++ Assets/Scripts/Components/Movement/UniformVerticalMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class UniformVerticalMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private float currentDirection = 0f;
+
+    /// <summary>
+    /// Indica si actualmente se está moviendo verticalmente.
+    /// </summary>
+    public bool IsMoving => Mathf.Abs(currentDirection) > 0.01f;
+    private Animator animator;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Define la dirección vertical del movimiento. -1: izquierda, 0: detenido, 1: derecha.
+    /// </summary>
+    public void Move(float direction)
+    {
+        currentDirection = Mathf.Clamp(direction, -1f, 1f);
+            animator.SetBool("isMoving", direction!=0);
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.linearVelocity;
+        velocity.y = currentDirection * moveSpeed;
+        rb.linearVelocity = velocity;
+    }
+}
