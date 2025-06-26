51f22673e409f005c0ea65d276178fe8f8f8bd56
JAYPU-11 <a20217327@pucp.edu.pe>
2025-06-26 17:33:33 -0500

    A

--- Assets/Scripts/Components/Platform/ArenaManager.cs
+++ Assets/Scripts/Components/Platform/ArenaManager.cs
@@ -1,35 +1,35 @@
-// using System.Collections;
-// using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
-// public class ArenaManager : MonoBehaviour
-// {
-//     public PlatformManager platformManager;
-//     private int platformCounter = 1;
+public class ArenaManager : MonoBehaviour
+{
+    public PlatformManagerLegacy platformManagerLegacy;
+    private int platformCounter = 1;
 
-//     // Puedes llamarlo desde otro script o incluso usar Start() si quieres que se llame al iniciar la escena
-//     void Start()
-//     {
-//         StartArena();
-//         StartCoroutine(SetPlatforms());
-//     }
-    
-    
-//     void StartArena()
-//     {
-//         platformManager.SpawnInitialPlatforms();
-//         //platformManager.DisablePlatform(1); // Desactiva una plataforma opcionalmente
-//     }
+    // Puedes llamarlo desde otro script o incluso usar Start() si quieres que se llame al iniciar la escena
+    void Start()
+    {
+        StartArena();
+        StartCoroutine(SetPlatforms());
+    }
 
-//     private IEnumerator SetPlatforms()
-//     {
-//         if (platformCounter < platformManager.allPlatforms.Count)
-//         {
-//             platformManager.OnWave(platformCounter);
-//             platformCounter++;
-//             Debug.Log(platformCounter);
-//         }
-//         else{ yield break;}
-//         yield return new WaitForSeconds(1);
-//         StartCoroutine(SetPlatforms());
-//     }
-// }
+
+    void StartArena()
+    {
+        platformManagerLegacy.SpawnInitialPlatforms();
+        //platformManagerLegacy.DisablePlatform(1); // Desactiva una plataforma opcionalmente
+    }
+
+    private IEnumerator SetPlatforms()
+    {
+        if (platformCounter < platformManagerLegacy.allPlatforms.Count)
+        {
+            platformManagerLegacy.OnWave(platformCounter);
+            platformCounter++;
+            Debug.Log(platformCounter);
+        }
+        else { yield break; }
+        yield return new WaitForSeconds(1);
+        StartCoroutine(SetPlatforms());
+    }
+}
